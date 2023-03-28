6feca9be0a7a5f9397e4d37a6971319a21a4cc12
Lamarr3314 <lamarrellis6@gmail.com>
2023-03-28 00:08:42 -0400

    the end is near good fellows

--- script.js
+++ script.js
@@ -2,12 +2,19 @@
 let mousePos = { x: undefined, y: undefined };
 let field = document.getElementById("fullField");
 let positionBox = document.getElementById("positions");
+let bumperLength = 10.5;
 let robot = document.getElementById("mainRobot");
 let robots = document.getElementById("robots");
 let play = document.getElementById("playSim");
 let positionText = document.getElementById("position");
 let changeColor = document.getElementById("redAlliance");
+let bumper = document.getElementById("bumper");
 let changeUnit = document.getElementById("isMeters");
+let customSub = document.getElementById("customSub");
+let customX = document.getElementById("customX");
+let customY = document.getElementById("customY");
+let customR = document.getElementById("customR");
+let customValues = [customX, customY, customR];
 let metersConversion = 1;
 let rotation = 0;
 let color = "blue";
@@ -19,8 +26,8 @@ let fieldLength = 622;
 let fieldHeight = 315;
 let maxWidth = 0;
 let maxHeight = 0;
-let robotLength = 33;
-let robotHeight = 27;
+let robotLength = 39;
+let robotHeight = 33;
 let listOfPoints = [];
 let finishedTranslation = true;
 let lastFinishedTranslation = false;
@@ -42,29 +49,84 @@ window.onload = function () {
     field.style.height = fieldHeight + "px";
     field.style.marginTop = (maxHeight - fieldHeight) / 2 + "px";
     field.style.marginLeft = (maxWidth - fieldLength) / 2 + "px";
+    robot.style.width = robotLength + "px";
+    robot.style.height = robotHeight + "px";
+    robot.style.textAlign = "center";
   }, 1100);
 };
-
+customSub.onclick = function () {
+  let x = null;
+  let y = null;
+  let r = null;
+  if (isValidPoint(customX.value)) {
+    x = Number(customX.value) * metersConversion;
+    console.log("x: " + x);
+    if (isValidPoint(customY.value)) {
+      y = -(Number(customY.value) * metersConversion);
+      console.log("y: " + y);
+      if (isValidPoint(customR.value)) {
+        r = Number(customR.value);
+        console.log("r: " + r);
+      }
+    }
+  }
+  if (x !== null && y !== null && r !== null) {
+    addPoint(x, y, r);
+    let newDiv = document.createElement("div");
+    newDiv.innerHTML = "<p>3314</p>";
+    robots.appendChild(newDiv);
+    moveRobot(x, y, r, newDiv);
+    customX.value = "";
+    customY.value = "";
+    customR.value = "";
+  }
+  if (x == null) {
+    alert("Please select a valid X point");
+    customX.value = "";
+  }
+  if (y == null) {
+    alert("Please select a valid Y point");
+    customY.value = "";
+  }
+  if (r == null) {
+    alert("Please select a valid rotation");
+    customR.value = "";
+  }
+};
 window.addEventListener("mousemove", (event) => {
   cX = Math.round(event.clientX - maxWidth / 2) - 9;
-  cY = Math.round(event.clientY - maxHeight / 2) - 9;
-  console.log(cY);
-  console.log("maxWidth" + maxWidth);
-  if (
-    Math.abs(cX) < fieldLength &&
-    Math.abs(cY) < fieldHeight / 2 - robotHeight / 2
-  ) {
-    if (cX > 237 + robotLength / 2) {
-      cX = 237 + robotLength / 2;
+  cY = Math.round(event.clientY - maxHeight / 2);
+  if (Math.abs(cX) < fieldLength && Math.abs(cY) < fieldHeight / 2) {
+    if (cX > 225 + robotLength / 2) {
+      cX = 229 + robotLength / 2;
     }
-    if (cX < -(237 + robotLength / 2)) {
-      cX = -(237 + robotLength / 2);
+    if (cX < -(225 + robotLength / 2)) {
+      cX = -(229 + robotLength / 2);
     }
+    if (cY > 136) {
+      cY = 140;
+    }
+    if (cY < -136) {
+      cY = -140;
+    }
+    // if(cY<112&& cY<-14){// charging station collision
+    //   if(cX<228 &&cX>116){
+
+    //   }
+    // }
+    // if (cX < 232) {
+    //   if (cX>232&&cX > 116) {
+    //     cX = 228;
+    //   }
+    //   else{
+    //     cX=116;
+    //   }
+    // }
     positionText.innerHTML =
       "(X: " +
       Math.round(100 * (cX / metersConversion)) / 100 +
       ", Y: " +
-      Math.round(100 * (cY / metersConversion)) / 100 +
+      -(Math.round(100 * (cY / metersConversion)) / 100) +
       ", R: " +
       rotation +
       ")";
@@ -77,7 +139,6 @@ window.addEventListener("mousemove", (event) => {
 });
 
 robot.onclick = function () {
-  console.log("clicked");
   addPoint(cX, cY, rotation);
   let newDiv = document.createElement("div");
   newDiv.innerHTML = "<p>3314</p>";
@@ -91,12 +152,11 @@ function addPoint(x, y, r) {
     "<p> new Pose2d(new Translate2d(" +
     Math.round(100 * (x / metersConversion)) / 100 +
     "," +
-    Math.round(100 * (y / metersConversion)) / 100 +
+    -(Math.round(100 * (y / metersConversion)) / 100) +
     "), new Rotation2d(" +
     r +
     "))</p>";
   positionBox.appendChild(newDiv);
-  console.log(listOfPoints);
   moveRobot(x, y, r, robot);
 }
 function playAnimation() {}
@@ -110,7 +170,6 @@ document.onkeydown = function (event) {
       break;
   }
   rotation %= 181;
-  console.log(rotation);
   moveRobot(cX, cY, rotation, robot);
 };
 function moveRobot(x, y, r, element) {
@@ -149,7 +208,6 @@ function simulate(i) {
     let directionX = -(listOfPoints[i].x - listOfPoints[i + 1].x);
     let directionY = -(listOfPoints[i].y - listOfPoints[i + 1].y);
     let directionR = -(listOfPoints[i].r - listOfPoints[i + 1].r);
-    console.log("X: " + directionX);
     let Xtime = (Math.abs(directionX) / 40) * xMPS;
     let Ytime = (Math.abs(directionY) / 40) * yMPS;
     let Rtime = ((Math.abs(directionR) * Math.PI) / 180) * rRPS;
@@ -167,14 +225,13 @@ function simulate(i) {
       "px) rotate(" +
       directionR +
       "deg)";
-    console.log();
     newDiv.addEventListener("transitionend", (event) => {
       newDiv.remove();
       if (i < listOfPoints.length - 2) {
         simulate(i + 1);
       }
     });
-  }, 50);
+  }, 10);
 }
 //-Add in simulation, div element goes away once the point is reached, this may mean adding in a robotsMade counter
 //convert points to how we use them, weird thing with x and y
